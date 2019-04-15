1c2bece989ed0d80c0cbc8fad6e4f7ebcd9137e6
pmpurifoy <pmpurifoy@gmail.com>
2019-04-14 23:51:53 -0500

    Twitter Bot finished

--- TwitterBot.py
+++ TwitterBot.py
@@ -1,5 +1,7 @@
+import sched, time
 import config
 import tweepy
+import json
 from tweepy.auth import OAuthHandler
 
 consumer_key = config.consumer_key
@@ -12,4 +14,18 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 user = api.me()
-print (user.name)
+s = sched.scheduler(time.time, time.sleep)
+numberOfTweets = 1
+def do_something(sc):
+    global numberOfTweets
+    try:
+        #api.update_status("@" + username + ' Harden is the best.', in_reply_to_status_id = tweet.id)
+        message = str(numberOfTweets) + '. James Harden is the MVP'
+        api.update_status(message)
+    except tweepy.TweepError as e:
+       print(e.reason)
+    numberOfTweets+=1
+    s.enter(43200, 1, do_something, (sc,))
+
+s.enter(0, 1, do_something, (s,))
+s.run()
